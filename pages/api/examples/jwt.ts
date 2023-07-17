2ff635c1ecbdccbd1e6724d940eac3e48f177f16
snygghugo <karlhugodahlgren@gmail.com>
2023-07-17 11:00:25 +0200

    Looks alright?

--- pages/api/examples/jwt.ts
+++ pages/api/examples/jwt.ts
@@ -1,7 +1,7 @@
 // This is an example of how to read a JSON Web Token from an API route
-import { getToken } from "next-auth/jwt"
+import { getToken } from 'next-auth/jwt';
 
-import type { NextApiRequest, NextApiResponse } from "next"
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(
   req: NextApiRequest,
@@ -9,6 +9,7 @@ export default async function handler(
 ) {
   // If you don't have the NEXTAUTH_SECRET environment variable set,
   // you will have to pass your secret as `secret` to `getToken`
-  const token = await getToken({ req })
-  res.send(JSON.stringify(token, null, 2))
+  const token = await getToken({ req });
+  console.log('hopefully this is token', token);
+  res.send(JSON.stringify(token, null, 2));
 }
