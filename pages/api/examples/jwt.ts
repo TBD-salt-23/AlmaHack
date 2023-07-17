7cdc855d0496370ace7dd4a93666242cb80ee0b8
Diana <diana.borro@appliedtechnology.se>
2023-07-17 15:41:51 +0200

    Feat: Events can be added

--- pages/api/examples/jwt.ts
+++ pages/api/examples/jwt.ts
@@ -1,17 +1,42 @@
 // This is an example of how to read a JSON Web Token from an API route
 import { getToken } from 'next-auth/jwt';
-import { getSession } from 'next-auth/react';
 import axios from 'axios';
 
 import type { NextApiRequest, NextApiResponse } from 'next';
+const BASE_URL = 'https://www.googleapis.com/calendar/v3/calendars';
+
+type GoogleTime = {
+  dateTime?: Date;
+  timeZone?: string;
+};
+
+type GoogleEventPost = {
+  start: GoogleTime;
+  end: GoogleTime;
+  summary: string;
+};
 
 let accessToken: any;
 
-const getNextWeekFromGoogle = async (pageToken = ''): Promise<any> => {
-  try {
-    console.log('this is the access token when GNWFG is called', accessToken);
+const addNewEvent = async (
+  googleEvent: GoogleEventPost,
+  calendarId: string
+) => {
+  const res = await axios.post(
+    `${BASE_URL}/${calendarId}/events`,
+    googleEvent,
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+  );
+};
 
+const getNextWeekFromGoogle = async (): Promise<any> => {
+  try {
     const currentDate = new Date();
+    //'2023-07-22T23:59:59.000Z'
     const nextWeekStart = new Date(
       currentDate.getFullYear(),
       currentDate.getMonth(),
@@ -22,8 +47,6 @@ const getNextWeekFromGoogle = async (pageToken = ''): Promise<any> => {
       currentDate.getMonth(),
       currentDate.getDate() + (7 - currentDate.getDay()) + 8
     );
-    console.log('this is nextweekstart', nextWeekStart);
-    console.log('this is nextweekend', nextWeekEnd);
 
     const { data } = await axios.get(
       // 'https://www.googleapis.com/calendar/v3/users/me/calendarList',
@@ -45,7 +68,6 @@ const getNextWeekFromGoogle = async (pageToken = ''): Promise<any> => {
     //   return data.items.concat(await getNextWeekFromGoogle(data.nextPageToken));
     // }
 
-    // return data.items;
     return data.items.map((event: any) => {
       return event.summary;
     });
@@ -60,15 +82,12 @@ export default async function handler(
   res: NextApiResponse
 ) {
   try {
-    // If you don't have the NEXTAUTH_SECRET environment variable set,
-    // you will have to pass your secret as `secret` to `getToken`
     const token = await getToken({ req });
     accessToken = token?.accessToken;
     const data = await getNextWeekFromGoogle();
 
     console.log('this is the data', data.join('\n'));
-    // console.log('hopefully this is token', token?.jti);
-    // const data = await getYTData();
+
     res.json(data);
   } catch (error) {
     res.json((error as Error).message);
