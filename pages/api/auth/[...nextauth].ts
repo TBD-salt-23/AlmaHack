e67192b30b2dfefbf0d297c92f1a1fa230b68778
snygghugo <karlhugodahlgren@gmail.com>
2023-07-17 13:51:29 +0200

    Accessing google calendar

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,22 +1,48 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+
+const clientId = process.env.GOOGLE_ID || '';
+const clientSecret = process.env.GOOGLE_SECRET || '';
+
 export const authOptions: NextAuthOptions = {
   // https://next-auth.js.org/configuration/providers/oauth
   providers: [
     GoogleProvider({
-      // clientId: process.env.GOOGLE_ID,
-      clientId:
-        '629586957237-6tdepdh786hh17srbt4li3kt0sc1s3g7.apps.googleusercontent.com',
-      // clientSecret: process.env.GOOGLE_SECRET,
-      clientSecret: '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',
+      clientId,
+      clientSecret,
+      authorization: {
+        params: {
+          scope:
+            'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
+        },
+      },
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   theme: {
-    colorScheme: 'light',
+    colorScheme: 'dark',
+  },
+  events: {
+    signIn(message) {
+      // console.log('signin message', message);
+    },
+  },
+  session: {
+    strategy: 'jwt',
+  },
+  jwt: {
+    maxAge: 30 * 24 * 60 * 60, // 30 days
   },
   callbacks: {
-    async jwt({ token }) {
+    async jwt({ token, user, account, profile }) {
       token.userRole = 'admin';
+      console.log('this is account?', account);
+      console.log('this is the user', user);
+      console.log('this is the profile', profile);
+      if (account) {
+        token.accessToken = account.access_token;
+        token.id = user.id;
+      }
       return token;
     },
   },
--- pages/api/examples/jwt.ts
+++ pages/api/examples/jwt.ts
@@ -1,15 +1,64 @@
 // This is an example of how to read a JSON Web Token from an API route
 import { getToken } from 'next-auth/jwt';
+import { getSession } from 'next-auth/react';
+import axios from 'axios';
 
 import type { NextApiRequest, NextApiResponse } from 'next';
+
+let accessToken: any;
+
+const getYTData = async (pageToken = ''): Promise<any> => {
+  try {
+    console.log(
+      'this is the access token when getYTdata is called',
+      accessToken
+    );
+    const { data } = await axios.get(
+      // 'https://www.googleapis.com/calendar/v3/users/me/calendarList',
+      'https://www.googleapis.com/calendar/v3/calendars/primary/events',
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+
+    // const { data } = await axios.get(
+    //   `https://youtube.googleapis.com/youtube/v3/subscriptions?mine=true&pageToken=${pageToken}&maxResults=50&part=snippet`,
+    //   {
+    //     headers: {
+    //       Authorization: `Bearer ${accessToken}`,
+    //     },
+    //   }
+    // );
+
+    if (data?.nextPageToken) {
+      return data.items.concat(await getYTData(data.nextPageToken));
+    }
+
+    return data.items;
+  } catch (error) {
+    console.log('this is the error', (error as Error).message);
+    return (error as Error).message;
+  }
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // If you don't have the NEXTAUTH_SECRET environment variable set,
-  // you will have to pass your secret as `secret` to `getToken`
-  const token = await getToken({ req });
-  console.log('hopefully this is token', token);
-  res.send(JSON.stringify(token, null, 2));
+  try {
+    // If you don't have the NEXTAUTH_SECRET environment variable set,
+    // you will have to pass your secret as `secret` to `getToken`
+    const token = await getToken({ req });
+    accessToken = token?.accessToken;
+    const data = await getYTData();
+
+    console.log('this is the data', data);
+    // console.log('hopefully this is token', token?.jti);
+    // const data = await getYTData();
+    res.json(data);
+  } catch (error) {
+    res.json((error as Error).message);
+  }
 }
